b3ca9f9da48f75c1e12025649fd86ddcd5b7d328
VillarrealScarlette <villarrealscarlette@gmail.com>
2019-06-23 14:59:49 -0400

    agregando proyecto cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,53 +1,53 @@
 window.cipher = {
-  encode: (number, textToCode) => {
-    let encodeMessage = "";
-    for (let i = 0; i < textToCode.length; i++) {
-      let codeAscii = textToCode.charCodeAt(i);
-      let newCodeAscii = 
-      //Letra ñ
-      codeAscii === 164 ? codeAscii
-      //Letro Ñ
-      : codeAscii === 165 ? codeAscii
-      //Mayúsculas
-      : codeAscii >= 65 && codeAscii <= 90
-      ? ((codeAscii-65+number)%26)+65
-      //Minúsculas
-      : codeAscii >= 97 && codeAscii <= 122
-      ? ((codeAscii-97+number)%26)+97
-      //Números
-      : codeAscii >= 48 && codeAscii <= 57
-      ? ((codeAscii-48+number)%10) + 48
-      //Para todo lo demás
-      : codeAscii;
-      let newChar = String.fromCharCode(newCodeAscii);
-      encodeMessage += newChar;
+    encode: (number, textToCode) => {
+      let encodeMessage = "";
+      for (let i = 0; i < textToCode.length; i++) {
+        let codeAscii = textToCode.charCodeAt(i);
+        let newCodeAscii = 
+        //Letra ñ
+        codeAscii === 164 ? codeAscii
+        //Letro Ñ
+        : codeAscii === 165 ? codeAscii
+        //Mayúsculas
+        : codeAscii >= 65 && codeAscii <= 90
+        ? ((codeAscii-65+number)%26)+65
+        //Minúsculas
+        : codeAscii >= 97 && codeAscii <= 122
+        ? ((codeAscii-97+number)%26)+97
+        //Números
+        : codeAscii >= 48 && codeAscii <= 57
+        ? ((codeAscii-48+number)%10) + 48
+        //Para todo lo demás
+        : codeAscii;
+        let newChar = String.fromCharCode(newCodeAscii);
+        encodeMessage += newChar;
+      }
+      return encodeMessage;
+    },
+    decode: (number, textToCode) => {
+      let decodeMessage = "";
+      let codeAscii = ""; 
+      for (let i = 0; i < textToCode.length; i++) {
+        codeAscii = textToCode.charCodeAt(i);
+        let newCodeAscii = 
+        //Letra ñ
+        codeAscii === 164 ? codeAscii
+        //Letro Ñ
+        :codeAscii === 165 ? codeAscii
+        //Mayúsculas
+        : codeAscii >= 65 && codeAscii <= 90
+        ? ((codeAscii-90-number)%26) + 90
+        //Minúsculas
+        : codeAscii >= 97 && codeAscii <= 122
+        ? ((codeAscii-122-number)%26) + 122
+        //Números
+        : codeAscii >= 48 && codeAscii <= 57
+        ? ((codeAscii-57-number)%10) + 57
+        //Para todo lo demás
+        : codeAscii;
+        let newChar = String.fromCharCode(newCodeAscii);
+        decodeMessage += newChar
+      }
+      return decodeMessage;
     }
-    return encodeMessage;
-  },
-  decode: (number, textToCode) => {
-    let decodeMessage = "";
-    let codeAscii = ""; 
-    for (let i = 0; i < textToCode.length; i++) {
-      codeAscii = textToCode.charCodeAt(i);
-      let newCodeAscii = 
-      //Letra ñ
-      codeAscii === 164 ? codeAscii
-      //Letro Ñ
-      :codeAscii === 165 ? codeAscii
-      //Mayúsculas
-      : codeAscii >= 65 && codeAscii <= 90
-      ? ((codeAscii-90-number)%26) + 90
-      //Minúsculas
-      : codeAscii >= 97 && codeAscii <= 122
-      ? ((codeAscii-122-number)%26) + 122
-      //Números
-      : codeAscii >= 48 && codeAscii <= 57
-      ? ((codeAscii-57-number)%10) + 57
-      //Para todo lo demás
-      : codeAscii;
-      let newChar = String.fromCharCode(newCodeAscii);
-      decodeMessage += newChar
-    }
-    return decodeMessage;
   }
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-/*Acá va tu código*/
+//Ocultar ventana de trabajo
+document.getElementById("windowWork").style.display="none";
+//Al clickear -Acceso Clientes-
+const btnCostumers = document.getElementById("costumers");
+btnCostumers.addEventListener("click", costumers);
+function costumers() {
+    document.getElementById("identification").style.display="none";
+    document.getElementById("requisite").style.display="none";
+    document.getElementById("infoRequisite").style.display="none";
+    document.getElementById("infoCopyRequisite").style.display="none";
+    document.getElementById("btnUncode").style.display="none";
+    document.getElementById("windowWork").style.display="block";
+}
+//Al clickear -Acceso Trabajador-
+const btnSupplier = document.getElementById("supplier");
+btnSupplier.addEventListener("click", supplier);
+function supplier() {
+    document.getElementById("identification").style.display="none";
+    document.getElementById("request").style.display="none";
+    document.getElementById("infoCopyText").style.display="none";
+    document.getElementById("infoRequest").style.display="none";
+    document.getElementById("btnCode").style.display="none";
+    document.getElementById("windowWork").style.display="block";
+}
+//Cliente accede para Codificar
+const btnCode = document.getElementById("code");
+btnCode.addEventListener("click", encode);
+function encode() {
+        let offset = document.getElementById("offset").value;
+        let number = parseInt(offset);
+        let textToCode = document.getElementById("enter_text").value;
+        if (offset == "") {
+                alert("No se ha ingresado RUT Cliente Empresa")
+        } else if (textToCode == "") {
+                alert("No se ha ingresado un requerimiento")
+        } else {
+                document.getElementById("out_text").value=window.cipher.encode(number, textToCode);
+        }        
+}
+//Trabajador accede para Decodificar
+const btnUncode = document.getElementById("uncode");
+btnUncode.addEventListener("click", decode);
+function decode() {
+        let offset = document.getElementById("offset").value;
+        let number = parseInt(offset);
+        let textToCode = document.getElementById("enter_text").value;
+        if (offset == "") {
+                alert("No se ha ingresado RUT Cliente Empresa")
+        } else if (textToCode == "") {
+                alert("No se ha ingresado un requerimiento de cliente")
+        } else {
+                document.getElementById("out_text").value=window.cipher.decode(number, textToCode);
+        }
+}
+//Boton Limpiar campos de texto
+const btnClear = document.getElementById("clear");
+btnClear.addEventListener("click", clear);
+function clear() {
+        document.getElementById("offset").value = "";
+        document.getElementById("enter_text").value = "";
+        document.getElementById("out_text").value = "";
+}
+//Boton Copiar texto
+const btnCopy = document.getElementById("copy");
+btnCopy.addEventListener("click", copy);
+function copy() {
+        let copyOutText = document.getElementById("out_text");
+        copyOutText.select();
+        document.execCommand("copy");
+} 
